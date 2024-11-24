ab9cc15c6c2f7dd502fcabeebe73b22b7468eb83
M <157221259+Mmdd93@users.noreply.github.com>
2024-11-24 19:35:05 +0200

    Update node.sh

--- node.sh
+++ node.sh
@@ -5111,11 +5111,6 @@ clear
 }
 
 display_system_info() {
-	
-
-# Function to display server location and IP
-    echo -e "${GREEN}Location:${NC} $SERVER_COUNTRY "
-    echo -e "${GREEN}Datacenter:${NC} $SERVER_ISP"
     echo -e "\n\033[1;31mOS info:\033[0m"
     echo -e "\033[1;32mOS:\033[0m $(lsb_release -d | cut -f2)"
     echo -e "\033[1;32mSERVER ISP:\033[0m $(curl -sS "http://ipwhois.app/json/$SERVER_IP" | jq -r '.isp')"
