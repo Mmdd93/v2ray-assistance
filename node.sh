99b685fa10e4629bc2579e6fc6d68b4e47b49436
M <157221259+Mmdd93@users.noreply.github.com>
2025-03-01 02:42:34 +0330

    Update node.sh

--- node.sh
+++ node.sh
@@ -4292,7 +4292,7 @@ main_menu() {
     echo -e "\033[1;32m42.\033[0m Auto panel restart"
     echo -e "\033[1;31m+-----------------------------------------+\033[0m"
     echo -e "\033[1;31m0.\033[0m Exit"
- echo -e "\n "
+
     read -p "Enter your choice: " choice
     
     case $choice in
