ee2faa45f7bb4e6e3ced51c4e0ec6ef3d950685c
M <157221259+Mmdd93@users.noreply.github.com>
2025-03-02 11:51:00 +0330

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 # Function to prompt for input with default value
+echo_red() {
+    echo -e "\033[1;31m$1\033[0m"
+}
+
+echo_green() {
+    echo -e "\033[1;32m$1\033[0m"
+}
+
+echo_yellow() {
+    echo -e "\033[1;33m$1\033[0m"
+}
+
+echo_blue() {
+    echo -e "\033[1;34m$1\033[0m"
+}
+
+echo_magenta() {
+    echo -e "\033[1;35m$1\033[0m"
+}
+
+echo_cyan() {
+    echo -e "\033[1;36m$1\033[0m"
+}
+
+echo_white() {
+    echo -e "\033[1;37m$1\033[0m"
+}
+
 prompt_input() {
     local prompt="$1"
     local default_value="${2:-}"
