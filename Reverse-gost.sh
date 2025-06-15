bc617725bee1bc89d0bf2ac4f6fe4554dbd4ba81
M <157221259+Mmdd93@users.noreply.github.com>
2025-06-16 01:30:56 +0330

    Update Reverse-gost.sh

--- Reverse-gost.sh
+++ Reverse-gost.sh
@@ -482,7 +482,7 @@ configure_socks5() {
             # Construct multi-port -L parameters
             GOST_OPTIONS=""
             for lport in "${lport_array[@]}"; do
-                GOST_OPTIONS+="-L ${LISTEN_TRANSMISSION}://:${lport}/127.0.0.1:${lport}"
+                GOST_OPTIONS+=" -L ${LISTEN_TRANSMISSION}://:${lport}/127.0.0.1:${lport}"
             done
             
 
