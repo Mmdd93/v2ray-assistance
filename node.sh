f3def59b1be4040f0773bb87a59f275f403de227
M <157221259+Mmdd93@users.noreply.github.com>
2025-06-30 15:40:11 +0330

    Update node.sh

--- node.sh
+++ node.sh
@@ -3253,13 +3253,14 @@ main_menu() {
     echo -e "\033[1;32m45.\033[0m GENEVE tunnel (IPV4 local)"
     echo -e "\033[1;32m46.\033[0m VXLAN tunnel (IPV4 local)"
     echo -e "\033[1;32m31.\033[0m HAProxy tunnel"
-    echo -e "\033[1;32m27.\033[0m Backhaul tunnel"
+    echo -e "\033[1;32m27.\033[0m Backhaul tunnel (https://github.com/Musixal/)"
     echo -e "\033[1;32m44.\033[0m GOST tunnel"
     echo -e "\033[1;32m55.\033[0m GOST Reverse tunnel"
     echo -e "\033[1;32m47.\033[0m WSS,WS tunnel (CDN support)"
     echo -e "\033[1;32m56.\033[0m WireGuard Reverse Tunnel (EYLAN Tunnel )"
     echo -e "\033[1;32m58.\033[0m WireGuard/hysteria Tunnel ((TAQ-BOSTAN) )"
     echo -e "\033[1;32m61.\033[0m Backhaul premium tunnel (cracked by https://t.me/anony_identity) "
+    echo -e "\033[1;32m62.\033[0m Rathole tunnel v2 (https://github.com/Musixal/) "
    
     echo -e "\n\033[1;31m+---------------Xray panel-----------------+\033[0m"
     echo -e "\033[1;32m38.\033[0m X-UI panel (x-ui 3x-ui tx-ui)"
@@ -3422,7 +3423,9 @@ main_menu() {
             curl -Ls https://raw.githubusercontent.com/wafflenoodle/zenith-stash/refs/heads/main/backhaul.sh -o backhaul-cracked.sh
             sudo bash backhaul-cracked.sh ;;  
 
-
+62) echo "Running ..."
+            curl -Ls https://raw.githubusercontent.com/Musixal/Rathole-Tunnel/refs/heads/main/rathole_v2.sh -o rathole_v2.sh
+            sudo bash rathole_v2.sh ;;  
        	00) echo "updating...";
 	update ;;
 	0) echo "Exiting...";
