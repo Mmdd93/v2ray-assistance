a42f757c2e714b2f83309b357145845b0f32b66c
M <157221259+Mmdd93@users.noreply.github.com>
2025-09-05 21:12:12 +0330

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -422,11 +422,11 @@ get_wildcard_ssl_with_certbot() {
 
         # Inform the user about the manual DNS challenge
     echo -e "\033[1;33mNote:\033[0m This process requires you to manually add DNS TXT records for domain verification."
-    sleep 1
+    
     echo -e "\033[1;32mCertbot will prompt you to create a TXT record for each domain.\033[0m"
-    sleep 1
+    
     echo -e "\033[1;32mYou will need to log into your DNS provider's control panel and add the TXT records.\033[0m"
-    sleep 1
+    
     echo -e "\033[1;34mPress Enter when you're ready to continue...\033[0m"
     read -r  # Wait for the user to press Enter
 
